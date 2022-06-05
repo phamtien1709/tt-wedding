cedf3315f39978c7b357dc94813ff3c404f4757e
Tien Pham <phamtien1709@gmail.com>
2022-06-05 23:49:51 +0700

    change background

--- src/components/FirstView.js
+++ src/components/FirstView.js
@@ -20,7 +20,7 @@ const StyledWrapper = styled.section`
   position: relative;
   width: 100%;
   height: 100vh;
-  background-image: url('https://www.brides.com/thmb/DeHRIBvUOg3IwBQuZW6cmAu0aNE=/2500x2500/filters:fill(auto,1)/sq-13f788e2730d4979b2baac6ca7c1cd6c.jpg');
+  background-image: url('https://drive.google.com/file/d/135SPJBi2cn7BpDhLpwmuh6EiwQysWG0O/view?usp=sharing');
   background-repeat: no-repeat;
   background-size: cover;
   background-position: center;
